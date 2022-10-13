ab3df9fa5701b1578592b740b7705612396ca063
Harshal Vijaywargiya <100298342+harshal144@users.noreply.github.com>
2022-10-13 18:37:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,23 +20,8 @@ const appandInTable = (users) =>{
     var txt2=document.createTextNode(users[i]['Description']);
     td2.appendChild(txt2);
 
-    var td3=document.createElement("td");
-    var txt3=document.createTextNode(users[i]['Sale_price']);
-    td3.appendChild(txt3);
-
-    var td4=document.createElement("td");
-    var txt4=document.createTextNode(users[i]['Regular_price']);
-    td4.appendChild(txt4);
-    
-
-    var td5=document.createElement("td");
-    var txt5=document.createTextNode(users[i]['Category']);
-    td5.appendChild(txt5);
     tr.appendChild(td1);
     tr.appendChild(td2);
-    tr.appendChild(td3);
-    tr.appendChild(td4);
-    tr.appendChild(td5);
     
     tbody.appendChild(tr);
   }
